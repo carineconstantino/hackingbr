76c7a719f5a57da0b944ccccf638a5ad9a530157
constantino <53983340+carineconstantino@users.noreply.github.com>
2023-11-12 23:48:50 -0300

    Update xss_encode.py

--- xss_encode/xss_encode.py
+++ xss_encode/xss_encode.py
@@ -1,1 +1,44 @@
-teste
+#!/usr/bin/python3
+
+import urllib.parse
+import requests
+import re
+import argparse
+import pyfiglet
+
+titulo = pyfiglet.figlet_format("XSS Encode")
+print(titulo)
+print('Criado por @hackingbr\n')
+print('-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_')
+
+if __name__ == '__main__':
+
+    program_name  = argparse.ArgumentParser(description = 'Mini-XSS-Test')
+    program_name.add_argument('--url', action='store', dest='url',
+                                            required = True, help = ''' Informe uma URL para executar o attack-test :::
+                                            Exemplo: python3 mini_xss_test.py --target https://example.com.br ''')
+    program_name.add_argument('--param', action='store', dest='param',
+                                            required = True, help = ''' Informe uma URL para executar o attack-test :::
+                                            Exemplo: python3 mini_xss_test.py --target https://example.com.br ''')
+                                                        
+    argumentos_parser = program_name.parse_args()
+    base_url = argumentos_parser.url
+    params = argumentos_parser.param
+
+
+file_path = 'payloads.txt'
+open_file = open(file_path, 'r')
+for line in open_file.readlines():
+    encoded_values = requests.utils.quote(line)
+    p = { params: encoded_values}
+    req = requests.get(base_url, params=p)
+    response = req.text
+    search = re.findall('teste-xss', response)
+    result_search = search
+    if len(search) > 0:
+        print('\nPayload Executado - XSS Attack Refleted ', req.status_code)
+        print('Payload: ', req.url)
+    else:
+        print('\nPayload não executado', req.status_code)
+        print('Payload enviado: ', req.url)
+
